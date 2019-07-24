5ea3bda94407b5df1c8f3e42baf46bd5dd18819a
Roger Abad <roger.abad@aqualogybs.net>
2019-07-24 14:38:28 +0200

    init

---
+++ src/main/java/com/walla/rover/api/roverApi/RoverApiApplication.java
@@ -0,0 +1,13 @@
+package com.walla.rover.api.roverApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoverApiApplication.class, args);
+	}
+
+}
